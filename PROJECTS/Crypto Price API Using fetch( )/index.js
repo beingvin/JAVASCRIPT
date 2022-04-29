f39690d792cf03120ffdeeedb16459d510dbf385
beingvin <beingvini@gmail.com>
2022-04-29 20:04:45 +0530

    Crypto Price API Using fetch( ) | code restructuring

--- PROJECTS/Crypto Price API Using fetch( )/index.js
+++ PROJECTS/Crypto Price API Using fetch( )/index.js
@@ -8,24 +8,19 @@ const options = {
   },
 };
 
-///////////////////// fetch method
+///////////////////// fetch get method
 
 fetch(
   "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0",
   options
-)
+) 
   .then((response) => response.json())
   .then((response) => {
-    ///////////////////// Vareables
+    /////////////////////  get div id's
     var title = document.getElementById("title");
     var crypto = document.getElementById("crypto");
-    // var cryptoRank = document.getElementById("cRank");
-    // var cryptoName = document.getElementById("cName");
-    // var cryptoPrice = document.getElementById("cPrice");
-    // var cryptoColor = document.getElementById("cColor");
-    // var cryptoChange = document.getElementById("cChange");
-    // var cryptoIcon = document.getElementById("cIcon");
 
+     ///////////////////// Insert titles to title div
     let wTitle = `<h3>Rank</h3> <h3>Name</h3>  
     <h3> Price (USD) </h3> 
     <h3> Change </h3>
@@ -34,20 +29,24 @@ fetch(
 
     title.innerHTML += wTitle;
 
-    console.log(response.data.coins[0]);
+    // console.log(response.data.coins[0]);
 
+    
+     ///////////////////// Get Coins Data
     let result = response.data.coins;
 
+     ///////////////////// Loop Coins Data to get Sigle Coin Data
     for (x in result) {
+
       let cName = result[x].name;
       let cPrice = result[x].price;
       let cChange = result[x].change;
       let cRank = result[x].rank;
       let cSymbol = result[x].symbol;
       let cIconURL = result[x].iconUrl;
-      let cColor = result[x].color;
+      // let cColor = result[x].color;
 
-      // console.log(cIcon);
+     
       let img = document.createElement("IMG");
       img.style.maxWidth = "50px";
       // img.setAttribute("src", cIconURL);
@@ -58,41 +57,13 @@ fetch(
                     <img  src="${cIconURL}" alt="image">
                     <h3> ${cSymbol}</h3> `;
 
+     ///////////////////// Insert crypto data to crypto div
+
       crypto.innerHTML += dWrite;
 
-      ///////////////////// Insert HTML
-      // document.write(dWrite);
-      // cryptoRank.innerHTML += cRank + "<br>";
-      // cryptoName.innerHTML += cName + "<br>";
-      // cryptoChange.innerHTML += cChange + "<br>";
-      // cryptoPrice.innerHTML += cPrice + "<br>";
-      // // cryptoIcon.scr(cIconURL);
-      // cryptoColor.style.backgroundColor += cColor;
-      // cryptoPrice.innerHTML = x;
+ 
 
-      // console.log(cryptoIcon.src);
-
-      // let div = document.createElement("div");
-      // document.body.append(div);
-
-      // div.append(document.body.append(img));
     }
   })
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(err));  ///////////////////// Log Errors
 
-// let htmlOutput = document.querySelector(".htmlOutput");
-// let img =
-//   "<img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.fastcompany.com%2F3034007%2Fwhats-the-difference-between-a-logo-and-a-symbol&psig=AOvVaw3TD4ncAjD0oZVftfhsIopL&ust=1651312944577000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCLDeneWBufcCFQAAAAAdAAAAABAi' alt=''>";
-// // img.src =
-// //   img.src +
-// //   "";
-
-// for (i = 0; i < 5; i++) {
-//   let hello = "<h1>Hello World</h1>";
-//   let img = document.createElement("IMG");
-//   img.setAttribute(
-//     "src",
-//     "https://www.tailorbrands.com/wp-content/uploads/2020/07/twitter-logo.jpg"
-//   );
-//   htmlOutput.innerHTML += hello;
-// }
